852fad03021cc0ccd45cbca144af34ae00dd5f19
krauchenco <krauchenco@gmail.com>
2014-05-11 13:33:32 -0300

    fix to express 4

--- server.js
+++ server.js
@@ -1,28 +1,61 @@
+// server.js (Express 4.0)
 var express = require('express'),
-  routes = require('./lib/routes'),
-  util = require('./lib/util'),
-  http = require('http'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
   app = express(),
-  server = http.createServer(app);
+  routes = require('./lib/routes'),
+  util = require('./lib/util')
+  ;
 
-conf = util.getJsonOrDie(__dirname+"/conf.json", "env", "port",
-  {env:"prod", port: "80"});
+var conf = util.getJsonOrDie(__dirname + "/conf.json", "env", "port",
+  {env: "prod", port: "80"});
 
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  var erroropts = { dumpExceptions: true, showStack: true };
-  if (conf.env === 'prod') {
-    erroropts = {};
-  }
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler(erroropts));
-});
+
+app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 					// log every request to the console
+app.use(bodyParser()); 						// pull information from html in POST
+app.use(methodOverride()); 					// simulate DELETE and PUT
 
 app.get('/', routes.index);
 
-server.listen(conf.port);
+var http = require('http');
+var server = http.createServer(app);
 
-console.log('listening on port '+server.address().port);
-var reloader = require('./lib/reloader')(server, __dirname+'/public');
+//app.listen(8080);
+server.listen(8080);
+
+console.log('Magic happens on port 8080'); 			// shoutout to the user
+
+var reloader = require('./lib/reloader')(server, __dirname + '/public');
+
+
+//////////////
+//var express = require('express'),
+//  routes = require('./lib/routes'),
+//  util = require('./lib/util'),
+//  http = require('http'),
+//  app = express(),
+//  server = http.createServer(app);
+//
+//conf = util.getJsonOrDie(__dirname + "/conf.json", "env", "port",
+//  {env: "prod", port: "80"});
+//
+////app.configure(function () {
+//app.set(express.bodyParser());
+//app.set(express.methodOverride());
+//app.set(app.router);
+////  var erroropts = { dumpExceptions: true, showStack: true };
+////  if (conf.env === 'prod') {
+////    erroropts = {};
+////  }
+//app.set(express.static(__dirname + '/public'));
+//app.set(express.errorHandler({ dumpExceptions: true, showStack: true }));
+////});
+//
+//app.get('/', routes.index);
+//
+//server.listen(conf.port);
+//
+//console.log('listening on port ' + server.address().port);
+//var reloader = require('./lib/reloader')(server, __dirname + '/public');
